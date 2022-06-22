a5cff1de83a397f97f0b8f56276f2ee49b0ae5ff
Dibrary <kkdh828@hanmail.net>
2022-06-22 18:20:22 +0900

    220622 템플릿 적용 테스트 완료

--- datadashboard/settings.py
+++ datadashboard/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-687ii9i51vbqhziw(x&e1f2yv(gw(m$zrj)qt-08$+)ekn&*h^'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
     'read_db_data.apps.ReadDbDataConfig'
 ]
 
@@ -95,7 +96,7 @@ USE_TZ = False
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
